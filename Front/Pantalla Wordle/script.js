bad897b21f6b3d1f2203c9e977d17f19f4c2d4cc
astillax <marcos.flomenboim@gmail.com>
2024-09-01 18:09:19 -0300

    1

--- Front/Pantalla Wordle/script.js
+++ Front/Pantalla Wordle/script.js
@@ -1,6 +1,6 @@
 
 const filas = document.querySelectorAll('.row');
-var finalDiv = document.getElementById("final");
+let finalDiv = document.getElementById("final"); // cambiado de var a let
 let filaActual = 0;
 let letraActual = 0;
 
@@ -10,14 +10,31 @@ let mensajeFinal = '';
 
 let listaPalabras = [];
 
-
-fetch('palabras2.txt')
-    .then(response => response.text())
+fetch('palabras.txt')
+    .then(response => response.text())    
     .then(data => {
         listaPalabras = data.split('\n').map(palabra => palabra.trim().toUpperCase());
         palabraCorrecta = listaPalabras[Math.floor(Math.random() * listaPalabras.length)];
-        console.log(`Palabra correcta: ${palabraCorrecta}`);
-    });
+        console.log(`Palabra correcta: ${palabraCorrecta}`);    
+    })    
+    .catch(err => console.log('Se produjo un error', err));
+
+ /* function buscarCaracteresDuplicados(str) {
+    var result = [];
+    var count = 0;
+
+    for (var i = 0; i < str.length; i++) {        
+        for (var j = i + 1; j < str.length; j++) {
+            console.log(`i: "${str[i]}"`);
+            console.log(`j: "${str[j]}"`);
+            if (str[i] === str[j]) {
+                result.push(str[i]);
+            }
+        }
+    }
+
+    return result;
+}  */
 
 const confirmarPalabra = () => {
     if (letraActual != 5) return;
@@ -25,20 +42,29 @@ const confirmarPalabra = () => {
     const letras = filas[filaActual].querySelectorAll('.letra');
     let palabraIngresada = Array.from(letras).map(letter => letter.textContent).join('');
 
-    if (!listaPalabras.includes(palabraIngresada)) {
-        alert('La palabra ingresada no existe en el diccionario.');
+    if (!listaPalabras.includes(palabraIngresada)) {      
+        mensajeFinal = 'La palabra ingresada no existe en el diccionario.'; // en lugar del alert
+        finalDiv.innerHTML = mensajeFinal;
         return;
     }
 
-    let sum = 0;
+    let sum = 0;    
+
+    // let duplicados = buscarCaracteresDuplicados(palabraCorrecta);
+    // console.log(duplicados);
+
     letras.forEach((letter, index) => {
-        letter.classList.add('checked');
+        console.log(letter);
+        letter.classList.add('checked');        
         const tecla = document.querySelector(`.key[data-key="${letter.textContent.toLowerCase()}"]`);
-        if (letter.textContent === palabraCorrecta[index]) {
+        if (letter.textContent === palabraCorrecta[index]) { // si la letra actual, está en la palabra buscada y en la posición correcta, se pinta de verde
             letter.style.backgroundColor = 'green';
             if (tecla) tecla.style.backgroundColor = 'green';
             sum++;
-        } else if (palabraCorrecta.includes(letter.textContent)) {
+        } else if (palabraCorrecta.includes(letter.textContent)) { // si la letra actual, está en la palabr buscada pero no es la posición correcta, se pinta de amarillo
+            // si la letra está más de una vez
+            /* if (palabraCorrecta.indexOf(letter.textContent) !== palabraCorrecta.lastIndexOf(letter.textContent)) {                
+            }; */
             letter.style.backgroundColor = 'yellow';
             if (tecla && tecla.style.backgroundColor !== 'green') {
                 tecla.style.backgroundColor = 'yellow';
@@ -71,6 +97,8 @@ document.addEventListener('keydown', e => {
     } else if (e.key === 'Backspace' && letraActual > 0) {
         letraActual--;
         letras[letraActual].textContent = '';
+        mensajeFinal = '';
+        finalDiv.innerHTML = mensajeFinal;
     } else if (e.key.match(/^[a-zñ]$/i) && letraActual < 5) {
         letras[letraActual].textContent = e.key.toUpperCase();
         letraActual++;
